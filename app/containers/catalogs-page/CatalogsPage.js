79ea73f3df6080ecb4300e92fb06f3cfc35ba659
Stepan Nikulenko <stepan.nikulenko@mediamonks.com>
2018-11-06 22:25:51 -0300

    name and catalogs layout fix

--- app/containers/catalogs-page/CatalogsPage.js
+++ app/containers/catalogs-page/CatalogsPage.js
@@ -9,60 +9,105 @@ const catalogsImages = [
   {
     id: 0,
     name: 'Rotating Machines',
-    path: require('../../static/images/catalogs/1.jpg')
+    path: './static/images/catalogs/0.jpg'
+    // path: require('../../static/images/catalogs/1.jpg')
   },
   {
     id: 1,
     name: 'Centro de Capacitación',
-    path: require('../../static/images/catalogs/1.jpg')
+    path: './static/images/catalogs/1.jpg'
+    // path: require('../../static/images/catalogs/1.jpg')
   },
   {
     id: 2,
     name: 'Superprofecionales',
-    path: require('../../static/images/catalogs/2.jpg')
+    path: './static/images/catalogs/2.jpg'
+    // path: require('../../static/images/catalogs/2.jpg')
   },
   {
     id: 3,
     name: 'Pastillas de Freno',
-    path: require('../../static/images/catalogs/3.jpg')
+    path: './static/images/catalogs/3.jpg'
+    // path: require('../../static/images/catalogs/3.jpg')
   },
-  { id: 4, name: 'Muscletools', path: require('../../static/images/catalogs/4.jpg') },
+  {
+    id: 4,
+    name: 'Muscletools',
+    path: './static/images/catalogs/4.jpg'
+    // require('../../static/images/catalogs/4.jpg')
+  },
   {
     id: 5,
     name: 'Repustos de Motor',
-    path: require('../../static/images/catalogs/5.jpg')
+    path: './static/images/catalogs/5.jpg'
+    // path: require('../../static/images/catalogs/5.jpg')
   },
-  { id: 6, name: 'Lámparas', path: require('../../static/images/catalogs/6.jpg') },
+  {
+    id: 6,
+    name: 'Lámparas',
+    path: './static/images/catalogs/6.jpg'
+    // path: require('../../static/images/catalogs/6.jpg')
+  },
   {
     id: 7,
     name: 'Kit de distribución',
-    path: require('../../static/images/catalogs/7.jpg')
+    path: './static/images/catalogs/7.jpg'
+    // path: require('../../static/images/catalogs/7.jpg')
   },
-  { id: 8, name: 'Filtros', path: require('../../static/images/catalogs/8.jpg') },
-  { id: 9, name: 'Escobillos', path: require('../../static/images/catalogs/9.jpg') },
+  {
+    id: 8,
+    name: 'Filtros',
+    path: './static/images/catalogs/8.jpg'
+    // path: require('../../static/images/catalogs/8.jpg')
+  },
+  {
+    id: 9,
+    name: 'Escobillos',
+    path: './static/images/catalogs/9.jpg'
+    // path: require('../../static/images/catalogs/9.jpg')
+  },
   {
     id: 10,
     name: 'Discos de freno',
-    path: require('../../static/images/catalogs/10.jpg')
+    path: './static/images/catalogs/10.jpg'
+    // path: require('../../static/images/catalogs/10.jpg')
   },
-  { id: 11, name: 'Diagnostics', path: require('../../static/images/catalogs/11.jpg') },
+  {
+    id: 11,
+    name: 'Diagnostics',
+    path: './static/images/catalogs/11.jpg'
+    // path: require('../../static/images/catalogs/11.jpg')
+  },
   {
     id: 12,
     name: 'Bujías Incandenscencia',
-    path: require('../../static/images/catalogs/12.jpg')
+    path: './static/images/catalogs/12.jpg'
+    // path: require('../../static/images/catalogs/12.jpg')
   },
-  { id: 13, name: 'Bujías', path: require('../../static/images/catalogs/13.jpg') },
+  {
+    id: 13,
+    name: 'Bujías',
+    path: './static/images/catalogs/13.jpg'
+    // path: require('../../static/images/catalogs/13.jpg')
+  },
   {
     id: 14,
     name: 'Bombas de combustible',
-    path: require('../../static/images/catalogs/14.jpg')
+    path: './static/images/catalogs/14.jpg'
+    // path: require('../../static/images/catalogs/14.jpg')
   },
   {
     id: 15,
     name: 'Bobinas de encendio',
-    path: require('../../static/images/catalogs/15.jpg')
+    path: './static/images/catalogs/15.jpg'
+    // path: require('../../static/images/catalogs/15.jpg')
   },
-  { id: 16, name: 'Baterías', path: require('../../static/images/catalogs/16.jpg') }
+  {
+    id: 16,
+    name: 'Baterías',
+    path: './static/images/catalogs/16.jpg'
+    // path: require('../../static/images/catalogs/16.jpg')
+  }
 ];
 
 type Props = {
@@ -78,8 +123,6 @@ export default class CatalogsPage extends Component {
     this.state = {
       selectedCatalogs: []
     };
-
-    
   }
 
   selectCatalog = id => {
